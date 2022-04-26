5626b0e5d455a95dc1d49e3fd47fa7cfc4f78514
BrandonHdez <99571569+BrandonHdez@users.noreply.github.com>
2022-04-25 20:51:34 -0700

    Add files via upload

---
+++ Clase_Mexa.cpp
@@ -0,0 +1,16 @@
+class mexicano 
+{
+
+    public String nombre;
+    public String apepat;
+    public String apemat;
+    public long RFC;
+    public long CURP;
+    public int  dianac;
+    public int  mesnac;
+    public int  yearnac;
+    public String  genero;
+    public String estado;
+ 
+ }
+ 
